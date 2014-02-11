7704be8546995196c9ea38bdcab9c222cc1e856f
Dirk Bretthauer <dirk.bretthauer@gmx.de>
2014-02-11 16:19:01 +0100

    The last fix was crap...

--- PgnReader/MovesSectionState.cs
+++ PgnReader/MovesSectionState.cs
@@ -141,7 +141,7 @@ namespace CChessCore.Pgn
                     _pgnMoves.Termination = termination;
                     _pgnMoves.MoveSection = new string(_stateBuffer.ToArray());
                     currentGame.AddMoves(_pgnMoves);
-                    ChangeState(this, _tagSectionState, null);
+                    ChangeState(this, _tagSectionState);
                     return PgnParseResult.EndOfGame;
                 }
                 else if (current == '\n')
--- PgnReader/PgnParserState.cs
+++ PgnReader/PgnParserState.cs
@@ -51,7 +51,7 @@ namespace CChessCore.Pgn
                 _stateBuffer.Clear();
             }
 
-            protected void ChangeState(PgnParserState oldState, PgnParserState newState, PgnMove currentMove)
+            protected void ChangeState(PgnParserState oldState, PgnParserState newState, PgnMove currentMove = null)
             {
                 this.OnExit();
                 _reader.SetState(newState);
